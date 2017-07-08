a081492ada2f359a16f32edbbc60ef9c64c7c69e
Lars Froelich <lars.froelich@gmail.com>
2017-07-08 12:48:07 +0200

    fully functional beta-version

--- index.js
+++ index.js
@@ -1,28 +1,46 @@
 #!/usr/bin/env node
 console.log("*** Merge-PDFs ***");
-console.log(" - loading dependencies");
-const pdf = require('pdfjs');
-const fs = require("fs");
+const cwd = process.cwd();
 
-console.log(" - loading font");
-console.log(__dirname);
+console.log("  - loading dependencies");
+const pdfjs = require('pdfjs');
+const fs = require("fs");
+const readdir = require("recursive-readdir")
+
+console.log("  - loading font");
 var helvetica = fs.readFileSync(__dirname + '/Helvetica.json');
 helvetica = JSON.parse(helvetica);
-var helveticaf = new pdf.Font(helvetica);
+var helveticaf = new pdfjs.Font(helvetica);
 
-console.log(" - loading pdfs");
-var pdf1 = fs.readFileSync(__dirname + '/pdf1.pdf');
-var pdf2 = fs.readFileSync(__dirname + '/pdf2.pdf');
+console.log("  - creating output pdf");
+var doc = new pdfjs.Document({font: helveticaf});
+
+console.log("  - adding PDFs");
+readdir(cwd, ["!*.pdf"]).then( // get all pdf-files
+    function(pdfs) {
+        pdfs.forEach(function(pdf){
+            console.log("  + adding \"" + pdf.replace(/^.*[\\\/]/, '') + "\"  (" + pdf + ")");
+            var file = fs.readFileSync(pdf);
+            var ext =  new pdfjs.ExternalDocument(file);
+            doc.addPagesOf(ext);
+        });
+        console.log("  - writing output-file");
+        doc.pipe(fs.createWriteStream(cwd + '/mergedpdf.pdf'))
+        doc.end().then(function () {
+            console.log("\n Finished writing PDF! Merged output is 'mergedpdf.pdf'");
+        });
+    },
+    function(error) {
+        console.error("Unable to search for pdf-files ", error);
+    }
+);
+/*var pdf1 = fs.readFileSync(cwd + '/pdf1.pdf');
+var pdf2 = fs.readFileSync(cwd + '/pdf2.pdf');
 var ext1 =  new pdf.ExternalDocument(pdf1);
 var ext2 =  new pdf.ExternalDocument(pdf2);
 
-console.log(" - creating output pdf");
+console.log("  - creating output pdf");
 var doc = new pdf.Document({font: helveticaf});
-
-console.log(" - adding pdfs to output pdf");
 doc.addPagesOf(ext1);
 doc.addPagesOf(ext2);
-
-console.log(" - write output to file");
-doc.pipe(fs.createWriteStream('output.pdf'))
-doc.end();
+*/
