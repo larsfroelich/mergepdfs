7811708a11cdd3b74baa249d7b70db4d316f3628
Lars Frölich <lars.froelich@gmail.com>
2025-06-10 15:16:57 +0200

    refactor: modernize index syntax

--- index.js
+++ index.js
@@ -1,65 +1,64 @@
 #!/usr/bin/env node
 
 const pkg = require('./package.json');
-const updateNotifier = require('update-notifier')({pkg});
-if(updateNotifier.update){
-    updateNotifier.notify({defer:false}); // display plz-update notification message
-    setTimeout(main, 5000); // start after 5 seconds
-}else{
-    main(); // up to date - start right away
+const updateNotifier = require('update-notifier')({ pkg });
+
+if (updateNotifier.update) {
+    updateNotifier.notify({ defer: false }); // display plz-update notification message
+    setTimeout(() => {
+        main().catch(err => console.error(err));
+    }, 5000); // start after 5 seconds
+} else {
+    main().catch(err => console.error(err)); // up to date - start right away
 }
 
-function main(){
-    console.log("v" + pkg.version + " - coded Nov17 by Lars Froelich\n");
-    console.log("*** Merge-PDFs ***");
+async function main() {
+    console.log(`v${pkg.version} - coded Nov17 by Lars Froelich\n`);
+    console.log('*** Merge-PDFs ***');
     const cwd = process.cwd();
 
     // configure program-parameters
-    var program = require('commander')
+    const program = require('commander')
         .option('-e, --even', 'Add empty pages to PDFs that end on odd pages (useful for manual duplex printing)')
         .parse(process.argv);
 
-    console.log("  - loading dependencies");
+    console.log('  - loading dependencies');
     const pdfjs = require('pdfjs');
-    const fs = require("fs");
-    const readdir = require("recursive-readdir");
+    const fs = require('fs');
+    const readdir = require('recursive-readdir');
 
-    console.log("  - loading font");
-    var helvetica = fs.readFileSync(__dirname + '/Helvetica.json');
-    helvetica = JSON.parse(helvetica);
-    var helveticaf = new pdfjs.Font(helvetica);
+    console.log('  - loading font');
+    const helvetica = JSON.parse(fs.readFileSync(`${__dirname}/Helvetica.json`));
+    const helveticaf = new pdfjs.Font(helvetica);
 
-    console.log("  - creating output pdf");
-    var doc = new pdfjs.Document({font: helveticaf});
+    console.log('  - creating output pdf');
+    const doc = new pdfjs.Document({ font: helveticaf });
 
-    var totalCount = 0;
-    var evenCount = 0;
-    console.log("  - adding PDFs");
-    readdir(cwd, ["!*.pdf", "merged.pdf"]).then( // get all pdf-files except previous merges
-        function(pdfs) {
-            pdfs.forEach(function(pdf){
-                console.log("  + adding \"" + pdf.replace(/^.*[\\\/]/, '') + "\"  (" + pdf + ")");
-                var file = fs.readFileSync(pdf);
-                var ext =  new pdfjs.ExternalDocument(file);
-                doc.addPagesOf(ext);
-                totalCount += ext.pageCount;
-                if(program.even && (ext.pageCount % 2 === 1)){
-                    doc.text(' . '); // some printers don't "skip" this page if it's entirely empty.
-                    totalCount ++;
-                    evenCount ++;
-                }
-            });
-            console.log("  - writing output-file");
-            doc.pipe(fs.createWriteStream(cwd + '/merged.pdf'));
-            doc.end().then(function () {
-                console.log("\n Finished writing PDF (" + totalCount + " pages total).\n Merged output is 'merged.pdf'");
-                if(program.even){
-                    console.log(" " + evenCount + " empty pages have been added to PDFs with odd numbers of pages.");
-                }
-            });
-        },
-        function(error) {
-            console.error("Unable to search for pdf-files ", error);
+    let totalCount = 0;
+    let evenCount = 0;
+    console.log('  - adding PDFs');
+    try {
+        const pdfs = await readdir(cwd, ['!*.pdf', 'merged.pdf']); // get all pdf-files except previous merges
+        pdfs.forEach(pdf => {
+            console.log(`  + adding "${pdf.replace(/^.*[\\/]/, '')}"  (${pdf})`);
+            const file = fs.readFileSync(pdf);
+            const ext = new pdfjs.ExternalDocument(file);
+            doc.addPagesOf(ext);
+            totalCount += ext.pageCount;
+            if (program.even && (ext.pageCount % 2 === 1)) {
+                doc.text(' . '); // some printers don't "skip" this page if it's entirely empty.
+                totalCount++;
+                evenCount++;
+            }
+        });
+        console.log('  - writing output-file');
+        doc.pipe(fs.createWriteStream(`${cwd}/merged.pdf`));
+        await doc.end();
+        console.log(`\n Finished writing PDF (${totalCount} pages total).\n Merged output is 'merged.pdf'`);
+        if (program.even) {
+            console.log(` ${evenCount} empty pages have been added to PDFs with odd numbers of pages.`);
         }
-    );
+    } catch (error) {
+        console.error('Unable to search for pdf-files ', error);
+    }
 }
